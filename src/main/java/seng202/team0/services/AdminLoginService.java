d8afb79bca3bbc34be4971ff64db5e894d78e9a9
fbl24 <flb24@uclive.ac.nz>
2024-08-13 14:20:46 +1200

    Created admin screen and admin login service

---
+++ src/main/java/seng202/team0/services/AdminLoginService.java
@@ -0,0 +1,14 @@
+package seng202.team0.services;
+
+public class AdminLoginService {
+
+    String hashedPassword = "";
+    String username = "";
+
+
+
+    public AdminLoginService(String password, String username) {
+
+    }
+
+}
