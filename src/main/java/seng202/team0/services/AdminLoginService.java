7ed28c90d82cc056ad6704d16a2053f7874b4a10
fbl24 <fbl24@uclive.ac.nz>
2024-08-19 13:54:02 +1200

    created needed files

---
+++ src/main/java/seng202/team0/services/AdminLoginService.java
@@ -0,0 +1,11 @@
+package seng202.team0.services;
+
+public class AdminLoginService {
+
+    public AdminLoginService() {
+
+    }
+
+    // check that login.txt is created
+
+}
--- src/main/java/seng202/team0/services/WineEnvironment.java
+++ src/main/java/seng202/team0/services/WineEnvironment.java
@@ -16,4 +16,5 @@ public class WineEnvironment {
     public void launchSetupScreen() {
         setupScreenLauncher.accept(this);
     }
+
 }
