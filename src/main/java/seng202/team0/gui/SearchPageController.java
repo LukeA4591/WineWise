6dbdd2dcfd8224aa5abd0d8256ad9b1af7d99224
Oliver Barclay <oba43@uclive.ac.nz>
2024-09-02 14:51:01 +1200

    Removed old flashy way to search through wines and started to remake how the search page side bar will work

--- src/main/java/seng202/team0/gui/SearchPageController.java
+++ src/main/java/seng202/team0/gui/SearchPageController.java
@@ -21,66 +21,10 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 
-public class SearchPageController implements Initializable {
-    @FXML
-    private GridPane grid;
-
-    @FXML
-    private ScrollPane scroll;
-
-    private Stage stage;
-
-    private int sizeOfRows = 2;
-    private int sizeOfColumns = 4;
-
-    private List<Wine> wines_displayed = new ArrayList<>();
+public class SearchPageController {
 
     private List<Wine> getData() {
-        List<Wine> wines_displayed = new ArrayList<>();
-        Wine wine;
-
-        for (int i = 0; i < 20; i++) {
-            wine = new Wine("Clean Skin");
-            wines_displayed.add(wine);
-        }
-
-        return wines_displayed;
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        wines_displayed.addAll(getData());
-        int column = 0;
-        int row = 0;
-        try {
-            for (int i = 0; i < wines_displayed.size(); i++) {
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("/fxml/wine_displayed.fxml"));
-
-                AnchorPane anchorPane = fxmlLoader.load();
-
-
-                DisplayedWineController displayedWineController = fxmlLoader.getController();
-                displayedWineController.setData(wines_displayed.get(i));
-
-
-                if (column == sizeOfColumns) {
-                    column = 0;
-                    row++;
-                }
-
-//                displayedWineController.setImageSize(100, 100);
-
-                grid.add(anchorPane, column++, row);
-                GridPane.setMargin(anchorPane, new Insets(15));
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void init(Stage stage){
-        this.stage = stage;
+        return null;
     }
 }
 
