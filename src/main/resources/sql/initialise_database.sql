a550fc9919cf83f6a7146cb2885cf9c50235cf81
Oliver Barclay <oba43@uclive.ac.nz>
2024-09-02 15:36:02 +1200

    added database manager classes from advanced javafx project

--- src/main/resources/sql/initialise_database.sql
+++ src/main/resources/sql/initialise_database.sql
@@ -1,20 +1,20 @@
 DROP TABLE IF EXISTS wine;
 --SPLIT
--- CREATE TABLE IF NOT EXISTS wine (
---      id INTEGER PRIMARY KEY AUTOINCREMENT,
---      item TEXT,
---      buyer_first_name TEXT,
---      buyer_last_name TEXT,
---      price REAL,
---      lat REAL,
---      lng REAL,
---      UNIQUE (lat, lng));
--- --SPLIT
--- DROP TABLE IF EXISTS users;
--- --SPLIT
--- CREATE TABLE IF NOT EXISTS users (
---      id INTEGER PRIMARY KEY AUTOINCREMENT,
---      username TEXT UNIQUE,
---      password TEXT);
--- --SPLIT
--- INSERT INTO users (username, password) VALUES ('admin', 'admin');
+CREATE TABLE IF NOT EXISTS wine (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     item TEXT,
+     buyer_first_name TEXT,
+     buyer_last_name TEXT,
+     price REAL,
+     lat REAL,
+     lng REAL,
+     UNIQUE (lat, lng));
+--SPLIT
+DROP TABLE IF EXISTS users;
+--SPLIT
+CREATE TABLE IF NOT EXISTS users (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     username TEXT UNIQUE,
+     password TEXT);
+--SPLIT
+INSERT INTO users (username, password) VALUES ('admin', 'admin');
