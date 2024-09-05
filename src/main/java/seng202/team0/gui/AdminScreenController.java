13ee6a2c57cbce49cf67f92b370f04a762d13b5f
Ryan Hamilton <rha206@uclive.ac.nz>
2024-09-05 22:50:23 +1200

    Search for and select csv file on device, added data type selector, merged add/edit/remove singular data into view table.

--- src/main/java/seng202/team0/gui/AdminScreenController.java
+++ src/main/java/seng202/team0/gui/AdminScreenController.java
@@ -1,16 +1,31 @@
 package seng202.team0.gui;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import seng202.team0.models.Wine;
 import seng202.team0.services.AdminLoginService;
 import seng202.team0.services.WineEnvironment;
 
+import java.io.File;
+import java.net.URISyntaxException;
+
 public class AdminScreenController {
     @FXML
     Button addWine;
+    @FXML
+    private ComboBox<String> dataTypeComboBox;
 
     private final WineEnvironment winery;
+    private Stage stage; // Make final and add void init(Stage stage)
+
+    // Could make it an init function with the stage.
+    public void initialize() {
+        dataTypeComboBox.setItems(FXCollections.observableArrayList("Wine", "Winery"));
+    }
 
     public AdminScreenController(WineEnvironment winery) {
         this.winery = winery;
@@ -20,5 +35,23 @@ public class AdminScreenController {
     public void onAddWine(){
         winery.getClearRunnable().run();
         winery.launchAddWineScreen();
+    }
+
+    /**
+     * Allows csv file to be chosen when the add dataset button is pressed.
+     */
+    @FXML
+    void addDataSet() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
+        try {
+            fileChooser.setInitialDirectory(new File(MainController.class.getProtectionDomain().getCodeSource()
+                    .getLocation().toURI()).getParentFile());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        stage = (Stage) addWine.getScene().getWindow(); // Need to have scene variable, has to find scene through addWine button
+        File file = fileChooser.showOpenDialog(stage);
     }
 }
