e238a896920e0123a2b0688cdb8f5ade63eee95c
Ryan Hamilton <rha206@uclive.ac.nz>
2024-09-06 17:09:01 +1200

    Gets values from text fields in wine and winery adder, toggle group getter doesn't work yet

--- src/main/java/seng202/team0/gui/AddWineController.java
+++ src/main/java/seng202/team0/gui/AddWineController.java
@@ -1,38 +1,36 @@
 package seng202.team0.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import seng202.team0.services.WineEnvironment;
 
 
 public class AddWineController {
     @FXML
+    TextField wineWineryName;
+    @FXML
     TextField wineName;
-
     @FXML
-    RadioButton typeWhite;
-
+    TextField wineVintage;
     @FXML
-    RadioButton typeRed;
-
-    @FXML
-    RadioButton typeRose;
-
-    @FXML
-    RadioButton typeSparkling;
-
-    @FXML
-    TextField wineWinery;
-
+    TextField wineScore;
     @FXML
     TextField wineRegion;
-
     @FXML
-    Button saveWine;
+    TextField wineDescription;
+    @FXML
+    RadioButton wineTypeWhite;
+    @FXML
+    RadioButton wineTypeRed;
+    @FXML
+    RadioButton wineTypeRose;
+    @FXML
+    ToggleGroup wineTypeToggle;
 
     private final WineEnvironment winery;
+
+
 
     public AddWineController(WineEnvironment winery) {
         this.winery = winery;
@@ -42,5 +40,20 @@ public class AddWineController {
     private void goBackToAdmin() {
         winery.getClearRunnable().run();
         winery.launchAdminScreen();
+    }
+
+    public void saveNewWine() {
+        try {
+            String wineWineryNameString = wineWineryName.getText();
+            String wineNameString = wineName.getText();
+            float wineVintageFloat = Float.parseFloat(wineVintage.getText());
+            int wineScoreInt = Integer.parseInt(wineScore.getText());
+            String wineTypeString = ((RadioButton) wineTypeToggle.getSelectedToggle()).getText();
+            String wineRegionString = wineRegion.getText();
+            String wineDescriptionString = wineDescription.getText();
+            System.out.println(wineTypeString);
+        } catch (NumberFormatException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
     }
 }
--- src/main/java/seng202/team0/gui/AddWineryController.java
+++ src/main/java/seng202/team0/gui/AddWineryController.java
@@ -1,9 +1,20 @@
 package seng202.team0.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import seng202.team0.services.WineEnvironment;
 
 public class AddWineryController {
+    @FXML
+    TextField wineryName;
+    @FXML
+    TextField wineryRegion;
+    @FXML
+    TextField wineryLatitude;
+    @FXML
+    TextField wineryLongitude;
+
     private final WineEnvironment winery;
 
     public AddWineryController(WineEnvironment winery) {
@@ -14,5 +25,16 @@ public class AddWineryController {
     private void goBackToAdmin() {
         winery.getClearRunnable().run();
         winery.launchAdminScreen();
+    }
+
+    public void saveNewWinery() {
+        try {
+            String wineryNameString = wineryName.getText();
+            String wineryRegionString = wineryRegion.getText();
+            float wineryLatitudeFloat = Float.parseFloat(wineryLatitude.getText());
+            float wineryLongitudeFloat = Float.parseFloat(wineryLongitude.getText());
+        } catch (NumberFormatException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
     }
 }
