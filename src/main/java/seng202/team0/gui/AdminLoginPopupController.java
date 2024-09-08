c71d8b6dbe713840fdcd70fda3b6432e2a15c6e9
lukearmstrong <lar93@uclive.ac.nz>
2024-09-09 09:14:12 +1200

    Rebasing homepage from master for searching functionality.

--- src/main/java/seng202/team0/gui/AdminLoginPopupController.java
+++ src/main/java/seng202/team0/gui/AdminLoginPopupController.java
@@ -1,7 +1,9 @@
 package seng202.team0.gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
@@ -29,6 +31,12 @@ public class AdminLoginPopupController {
     private TextField passwordInput;
 
     @FXML
+    private PasswordField passwordTextInput;
+
+    @FXML
+    private Button viewButton;
+
+    @FXML
     private Label adminLoginErrorLabel;
 
     /**
@@ -48,6 +56,9 @@ public class AdminLoginPopupController {
     public void init(WineEnvironment winery) {
         this.winery = winery;
         this.adminLoginInstance = winery.getAdminLoginInstance();
+        // Bind textField with passwordField
+        passwordInput.textProperty().bindBidirectional(passwordTextInput.textProperty());
+        passwordInput.setVisible(false);
         passwordInput.sceneProperty().addListener((observable, oldscene, newscene) -> {
             if (newscene != null) {
                 newscene.getStylesheets().add(getClass().getResource("/style/navbar.css").toExternalForm());
@@ -69,9 +80,24 @@ public class AdminLoginPopupController {
         if (!errorMessage.isEmpty()) {
             adminLoginErrorLabel.setText(errorMessage);
         } else {
+            // go to admin screen
+            // close controller
             ((Stage) usernameInput.getScene().getWindow()).close();
             winery.getClearRunnable().run();
             winery.launchAdminScreen();
+        }
+    }
+
+    @FXML
+    public void onViewButtonClicked() {
+        if (passwordInput.isVisible()) {
+            passwordInput.setVisible(false); // hide password
+            passwordTextInput.setVisible(true);
+            viewButton.setText("View");
+        } else {
+            passwordInput.setVisible(true); // show password
+            passwordTextInput.setVisible(false);
+            viewButton.setText("Hide");
         }
     }
 
--- src/main/java/seng202/team0/gui/HomePageController.java
+++ src/main/java/seng202/team0/gui/HomePageController.java
@@ -1,16 +1,61 @@
 package seng202.team0.gui;
 
+import seng202.team0.models.Wine;
+import java.util.List;
+import java.util.ArrayList;
+import javafx.fxml.FXML;
 import javafx.stage.Stage;
+import seng202.team0.repository.WineDAO;
+import javafx.scene.control.Label;
 
 public class HomePageController {
 
+    @FXML
+    private Label desc1;
+
+    @FXML
+    private Label desc2;
+
+    @FXML
+    private Label desc3;
+
+    @FXML
+    private Label wine1;
+
+    @FXML
+    private Label wine2;
+
+    @FXML
+    private Label wine3;
+
+
     private Stage stage;
 
+    private WineDAO wineDAO;
     /**
      * Init method for HomePageController
      * @param stage stage from NavBarController
      */
     public void init(Stage stage){
         this.stage = stage;
+        wineDAO = new WineDAO();
+        // displayWines();
+        // displayDescriptions();
     }
+
+    public void displayWines(){
+        List<Wine> wines = wineDAO.getAll();
+
+        wine1.setText(wines.get(0).getWineName());
+        wine2.setText(wines.get(1).getWineName());
+        wine3.setText(wines.get(2).getWineName());
+    }
+
+    public void displayDescriptions(){
+        List<Wine> wines = wineDAO.getAll();
+        desc1.setText(wines.get(0).getDescription());
+        desc2.setText(wines.get(1).getDescription());
+        desc3.setText(wines.get(2).getDescription());
+    }
+
 }
