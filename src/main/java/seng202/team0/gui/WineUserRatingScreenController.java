5698496030ef92da1e4a8dd488b7c2c21a79f2b2
Alex Wilson <awi218@uclive.ac.nz>
2024-09-15 17:40:48 +1200

    Made user rating screen

--- src/main/java/seng202/team0/gui/WineUserRatingScreenController.java
+++ src/main/java/seng202/team0/gui/WineUserRatingScreenController.java
@@ -1,17 +1,79 @@
 package seng202.team0.gui;
 
 import javafx.fxml.FXML;
-import seng202.team0.models.Wine;
-public class WineUserRatingScreenController {
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import seng202.team0.exceptions.DuplicateExc;
+import seng202.team0.models.Rating;
+import seng202.team0.models.Wine;
+import seng202.team0.repository.ReviewDAO;
+
+public class WineUserRatingScreenController {
     Wine wine;
+
+    @FXML
+    private Label currentWineLabel;
+    @FXML
+    private Label currentUserRatingLabel;
+    @FXML
+    private Label criticRatingLabel;
+    @FXML
+    private Slider ratingSlider;
+    @FXML
+    private TextArea reviewTextArea;
+    @FXML
+    private Button saveReviewButton;
+    @FXML
+    private Label savedLabel;
+    @FXML
+    private Button resetReviewButton;
+    private ReviewDAO reviewDAO = new ReviewDAO();
+    private boolean movedSlider = false;
+
 
     public WineUserRatingScreenController() {
     }
 
+    /**
+     * Init method for the user rating screen
+     * @param wine
+     */
     @FXML
     public void init(Wine wine) {
         this.wine = wine;
+        currentWineLabel.setText("Wine: " + wine.getWineName() + " from " + wine.getWineryString());
+        //TODO display current user rating
+        criticRatingLabel.setText("Critic Rating: " + wine.getScore());
     }
+
+    @FXML
+    private void saveReview() throws DuplicateExc {
+        if (movedSlider) {
+            Rating rating = new Rating((int) ratingSlider.getValue(), reviewTextArea.getText(), wine); //will break with new rating system
+            reviewDAO.add(rating);
+            savedLabel.setText("Saved!");
+            savedLabel.setStyle("-fx-text-fill: indigo");
+            resetReview();
+        } else {
+            savedLabel.setText("Please Move Slider");
+            savedLabel.setStyle("-fx-text-fill: red");
+        }
+    }
+
+    @FXML
+    private void resetReview() {
+        ratingSlider.setValue(0);
+        reviewTextArea.setText("");
+    }
+
+    @FXML
+    private void sliderMoved() {
+        movedSlider = true;
+    }
+
+
 
 }
