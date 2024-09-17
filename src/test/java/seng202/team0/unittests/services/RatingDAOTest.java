0641d26047d56ce42eea23bd6f65c958f712a04a
Alex Wilson <awi218@uclive.ac.nz>
2024-09-17 15:22:04 +1200

    Added a few test cases to RatingDAOTest

--- src/test/java/seng202/team0/unittests/services/RatingDAOTest.java
+++ src/test/java/seng202/team0/unittests/services/RatingDAOTest.java
@@ -17,6 +17,8 @@ public class RatingDAOTest {
     private static ReviewDAO reviewDao;
     private static WineDAO wineDAO;
     private static DatabaseManager databaseManager;
+    private Wine testWine1 = new Wine("Red", "Plume Pinot Noir", "Lake Chalice", 2019, 80, "Marlborough", "A yummy wine");
+    private Wine testWine2 = new Wine("White", "Plume Sav", "Lake Chalice", 2019, 85, "Marlborough", "So tasty");
 
     @BeforeAll
     public static void setup() throws DuplicateExc {
@@ -29,26 +31,17 @@ public class RatingDAOTest {
     @BeforeEach
     public void resetDB() {
         databaseManager.resetDB();
+        wineDAO.add(testWine1);
+        wineDAO.add(testWine2);
     }
 
     /**
      * Populates the test database with ratings
      */
     private void populateDatabase() throws DuplicateExc {
-        //reviewDao.add(new Rating(60, "I thought it was really good, I liked the color", testWine1));
-        //reviewDao.add(new Rating(90, "This blew my socks off", testWine1));
-        //reviewDao.add(new Rating(30, "Actually horrendous", testWine2));
-    }
-
-    @BeforeEach
-    /**
-     * Populates the test database with wines
-     */
-    public void populateDatabaseWithWine() throws DuplicateExc {
-        wineDAO.add(new Wine("Red", "Plume Pinot Noir", "Lake Chalice", 2019, 80, "Marlborough", "A yummy wine"));
-        wineDAO.add(new Wine("White", "Plume Sav", "Lake Chalice", 2019, 85, "Marlborough", "So tasty"));
-        wineDAO.add(new Wine("Rose", "Rosy Rose", "Lakes Winery", 2020, 90, "Otago", "Very rosy"));
-        wineDAO.add(new Wine("White", "Bland Blanc", "Fields of Grapes", 2021, 50, "Canterbury", "Bland and boring"));
+        reviewDao.add(new Rating(60, "I thought it was really good, I liked the color", testWine1));
+        reviewDao.add(new Rating(90, "This blew my socks off", testWine1));
+        reviewDao.add(new Rating(30, "Actually horrendous", testWine2));
     }
 
     @Test
@@ -56,6 +49,13 @@ public class RatingDAOTest {
         populateDatabase();
         List<Rating> ratings = reviewDao.getAll();
         Assertions.assertEquals(3, ratings.size());
+    }
+
+    @Test
+    public void testGetRatingsFromWine() throws DuplicateExc {
+        populateDatabase();
+        List<Rating> ratings = reviewDao.getReviewsByWineId(reviewDao.getWineID(testWine1));
+        Assertions.assertEquals(2, ratings.size());
     }
 
 }
