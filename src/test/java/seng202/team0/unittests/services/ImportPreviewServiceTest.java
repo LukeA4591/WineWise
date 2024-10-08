f0ab8f040553c7e878612b73aaa38e293fa86388
rha206 <rha206@uclive.ac.nz>
2024-10-08 18:05:33 +1300

    Started tests for ImportPreviewService's modifyHeaders method.

---
+++ src/test/java/seng202/team0/unittests/services/ImportPreviewServiceTest.java
@@ -0,0 +1,50 @@
+package seng202.team0.unittests.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seng202.team0.services.ImportPreviewService;
+
+public class ImportPreviewServiceTest {
+
+    private ImportPreviewService importPreviewService;
+
+    @BeforeEach
+    public void setup() {
+        importPreviewService = new ImportPreviewService();
+    }
+
+    private String[] createData() {
+        return new String[] {};
+    }
+
+    @Test
+    public void testAllValidModifyHeaders() {
+        String[] headers = new String[]{"First", "Second", "Third", "Fourth"};
+        String[] expectedHeaders = new String[]{"First", "Second", "Third", "Fourth"};
+        String[] modifiedHeaders = importPreviewService.modifyHeaders(headers);
+        Assertions.assertArrayEquals(expectedHeaders, modifiedHeaders);
+    }
+
+    @Test
+    public void testDuplicateOrEmptyModifyHeaders() {
+        String[] headers = new String[]{"First", "First", "Third", " "};
+        String[] expectedHeaders = new String[]{"First", "First_1", "Third", "Temp"};
+        String[] modifiedHeaders = importPreviewService.modifyHeaders(headers);
+        Assertions.assertArrayEquals(expectedHeaders, modifiedHeaders);
+    }
+
+    @Test
+    public void testDuplicateWithEmptyModifyHeaders() {
+        String[] headers = new String[]{"First", "First", "First_1", "", "Temp", "Temp_1"};
+        String[] expectedHeaders = new String[]{"First", "First_1", "First_1#", "Temp", "Temp_1", "Temp_1#"};
+        String[] modifiedHeaders = importPreviewService.modifyHeaders(headers);
+        Assertions.assertArrayEquals(expectedHeaders, modifiedHeaders);
+    }
+
+    @Test
+    public void testAllValidCheckHeaders() {
+
+    }
+
+}
