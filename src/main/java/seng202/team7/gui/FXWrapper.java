783ab4bbc5bcd9e92d4f60a7834f28e71d6c9062
Oliver Barclay <oba43@uclive.ac.nz>
2024-10-11 16:07:52 +1300

    Added Desktop icons (currently white but OG version available, not used as hard to see)

--- src/main/java/seng202/team7/gui/FXWrapper.java
+++ src/main/java/seng202/team7/gui/FXWrapper.java
@@ -3,11 +3,13 @@ package seng202.team7.gui;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import seng202.team7.services.AppEnvironment;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * The class allows pages to be launched when the current page is switched to another page.
@@ -33,6 +35,8 @@ public class FXWrapper {
      * @param appEnvironment The environment which manages the state of the application.
      */
     public void launchNavBar(final AppEnvironment appEnvironment) {
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/whiteIconBG(1).png")));
+        stage.getIcons().add(icon);
         try {
             FXMLLoader navBarLoader = new FXMLLoader(getClass().getResource("/fxml/nav_bar.fxml"));
             Parent setupParent  = navBarLoader.load();
