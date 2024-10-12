3a63f6dd13ebb057f9e46595c3a4f491aeed13cc
rha206 <rha206@uclive.ac.nz>
2024-10-12 18:53:03 +1300

    Added wineries sidebar and zoom to user map page

--- src/main/java/seng202/team7/gui/MapPageController.java
+++ src/main/java/seng202/team7/gui/MapPageController.java
@@ -1,7 +1,13 @@
 package seng202.team7.gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import netscape.javascript.JSObject;
@@ -10,6 +16,7 @@ import org.apache.logging.log4j.Logger;
 import seng202.team7.business.WineryManager;
 import seng202.team7.models.Winery;
 
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -17,13 +24,18 @@ public class MapPageController {
     private static final Logger log = LogManager.getLogger(seng202.team7.gui.MapPageController.class);
     @FXML
     private WebView webView;
+    @FXML
+    private ListView<Winery> wineryList;
+    @FXML
+    private TextField searchWinery;
     private WebEngine webEngine;
     private JSObject javaScriptConnector;
     private WineryManager wineryManager;
 
     void init() {
-        initMap();
         wineryManager = new WineryManager();
+        initMap();
+        setWineryList(wineryManager.getAll());
     }
 
     private void initMap() {
@@ -50,5 +62,54 @@ public class MapPageController {
 
     private void displayMarkers() {
         javaScriptConnector.call("displayMarkers");
+    }
+
+    void setWineryList(List<Winery> wineries) {
+        wineries.sort(Comparator.comparing(Winery::getWineryName));
+        ObservableList<Winery> wineryNames = FXCollections.observableArrayList(wineries);
+        wineryList.setItems(wineryNames);
+        wineryList.setCellFactory(lv -> {
+            ListCell<Winery> cell = new ListCell<>() {
+                @Override
+                protected void updateItem(Winery winery, boolean empty) {
+                    super.updateItem(winery, empty);
+                    if (empty || winery == null) {
+                        setText(null);
+                        setStyle("");
+                    } else {
+                        setText(winery.getWineryName());
+                        Winery selectedWinery = wineryList.getSelectionModel().getSelectedItem();
+                        if (winery == selectedWinery) {
+                            setStyle("-fx-background-color: #eccca2");
+                            if (winery.getLatitude() != null && winery.getLongitude() != null) {
+                                javaScriptConnector.call("zoomToLocation", winery.getLatitude(), winery.getLongitude(), 13);
+                            }
+                        } else if (winery.getLatitude() == null || winery.getLongitude() == null) {
+                            setStyle("-fx-background-color: #ffb3b3");
+                        } else {
+                            setStyle("-fx-background-color: #a6f2ad");
+                        }
+                    }
+                }
+            };
+            cell.hoverProperty().addListener((observer, wasHovered, isNowHovered) -> {
+                if (isNowHovered && !cell.isEmpty()) {
+                    cell.setStyle("-fx-background-color: #eccca2");
+                } else {
+                    cell.updateSelected(true);
+                }
+            });
+            return cell;
+        });
+    }
+
+    @FXML
+    void updateSearch() {
+        String wineryName = searchWinery.getText();
+        if (wineryName.isBlank()) {
+            setWineryList(wineryManager.getAll());
+        } else {
+            setWineryList(wineryManager.getAllLikeSearch(wineryName));
+        }
     }
 }
