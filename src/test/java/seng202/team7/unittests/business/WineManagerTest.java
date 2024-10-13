275430da954bd978fbf83325b8ea7ee57dd37b7b
rha206 <rha206@uclive.ac.nz>
2024-10-13 14:28:18 +1300

    Importer and manager unit tests for wines.

--- src/test/java/seng202/team7/unittests/business/WineManagerTest.java
+++ src/test/java/seng202/team7/unittests/business/WineManagerTest.java
@@ -7,19 +7,24 @@ import org.junit.jupiter.api.Test;
 import seng202.team7.business.WineManager;
 import seng202.team7.business.WineryManager;
 import seng202.team7.exceptions.DuplicateExc;
+import seng202.team7.io.Importable;
+import seng202.team7.io.WineCSVImporter;
 import seng202.team7.models.Wine;
 import seng202.team7.models.Winery;
 import seng202.team7.repository.DatabaseManager;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WineManagerTest {
 
     private static WineManager wineManager;
     private static DatabaseManager databaseManager;
-
-    Wine wine1;
 
     @BeforeAll
     public static void setup() throws DuplicateExc {
@@ -34,7 +39,7 @@ public class WineManagerTest {
     }
 
     private void populateDB() {
-        wine1 = new Wine("Red", "Noir", "Winery", 2000, 85, "REGION 1", "High quality :)");
+        Wine wine1 = new Wine("Red", "Noir", "Winery", 2000, 85, "REGION 1", "High quality :)");
         Wine wine2 = new Wine("White", "Not Noir", "Winery", 2001, 95, "REGION 1", "Higher quality :)");
         Wine wine3 = new Wine("Red", "Noir again", "Diff. Winery", 2010, 80, "REGION 2", "'aight");
 
@@ -59,5 +64,53 @@ public class WineManagerTest {
         anotherWineManager.add(new Wine("Red", "New wine to diff manager", "Winery", 2019, 75, "REGION 1", "secret"));
         Assertions.assertEquals(4, wineManager.getAll().size());
         Assertions.assertEquals(4, anotherWineManager.getAll().size());
+    }
+
+    @Test
+    public void testAddBatch() {
+        Importable<Wine> wineCSVImporter = new WineCSVImporter();
+        URL url = Thread.currentThread().getContextClassLoader().getResource("files/TestDecanter.csv");
+        File file = new File(url.getPath());
+        List<Integer> headerIndexes = Arrays.asList(5, 1, 0, 4, 2, 3, 6);
+        wineManager.addBatch(wineCSVImporter, file, headerIndexes);
+        List<Wine> wines = wineManager.getAll();
+        List<Wine> expectedWines = wineCSVImporter.readFromFile(file, headerIndexes);
+        Assertions.assertEquals(expectedWines.size(), wines.size());
+    }
+
+    @Test
+    public void testDeleteAndUpdateWines() {
+        Wine wine = new Wine("Red", "Pinot Noir", "Winery", 2000, 100, "Region", "Nice");
+        wineManager.add(wine);
+        Wine updateWine = new Wine("White", "Pinot Gris", "Winery", 2000, 100, "Region", "Nice");
+        wineManager.updateWine(updateWine, wine);
+        Assertions.assertTrue(wineManager.checkIfWineExists(updateWine));
+        Wine newWine = wineManager.getWineFromID(wineManager.getWineID(updateWine));
+        Assertions.assertEquals(updateWine, newWine);
+        wineManager.delete("Pinot Gris", "Winery", 2000);
+        Assertions.assertEquals(0, wineManager.getAll().size());
+    }
+
+    @Test
+    public void testGetDistinct() {
+        populateDB();
+        List<String> expectedNames = Arrays.asList("Noir", "Not Noir", "Noir again");
+        List<String> names = wineManager.getDistinct("name");
+        Assertions.assertEquals(new HashSet<>(names), new HashSet<>(expectedNames));
+    }
+
+    @Test
+    public void testSearchWines() {
+        populateDB();
+        List<Wine> wines = wineManager.searchWines("Noir");
+        Assertions.assertEquals(3, wines.size());
+    }
+
+    @Test
+    public void testGetWineWithWinery() {
+        populateDB();
+        List<Wine> expectedWines = wineManager.getAll().stream().filter(wine -> "Winery".equals(wine.getWineryString())).toList();
+        List<Wine> wines = wineManager.getWineWithWinery(new Winery("Winery"));
+        Assertions.assertEquals(expectedWines.size(), wines.size());
     }
 }
--- src/test/java/seng202/team7/unittests/io/WineImporterTest.java
+++ src/test/java/seng202/team7/unittests/io/WineImporterTest.java
@@ -13,8 +13,23 @@ import java.util.List;
 
 public class WineImporterTest {
 
+    public List<String[]> getSixLinesFromTestDecanter() {
+        return Arrays.asList(
+                new String[] {"Producer","Wine Name","Score","Region","Vintage","Colour","Description"},
+                new String[] {"Lake Chalice","Plume Pinot Noir","86","Marlborough","2017","Red",
+                        "A great improvement with Oaky notes and a hint of raspberries and peaches"},
+                new String[] {"Giesen Group","Clayvin Single Vineyard Chardonnay","94","Marlborough","2017","White",
+                        "A great improvement with Oaky notes and a hint of raspberries and peaches"},
+                new String[] {"Spy Valley","Envoy Outpost Pinot Noir","89","Marlborough","2017","Red",
+                        "A great improvement with Oaky notes and a hint of raspberries and peaches"},
+                new String[] {"Te Kano Estate","Northburn Chardonnay","91","Central Otago","2021","White",
+                        "A great improvement with Oaky notes and a hint of raspberries and peaches"},
+                new String[] {"Vidal","Reserve Chardonnay","88","Hawke's Bay","2021","White",
+                        "A great improvement with Oaky notes and a hint of raspberries and peaches"});
+    }
+
     @Test
-    public void readWinesFromFile() {
+    public void textReadWinesFromFile() {
         Importable<Wine> wineCSVImporter = new WineCSVImporter();
         URL url = Thread.currentThread().getContextClassLoader().getResource("files/TestDecanter.csv");
         File file = new File(url.getPath());
@@ -22,16 +37,16 @@ public class WineImporterTest {
         List<Wine> wines = wineCSVImporter.readFromFile(file, headerIndexes);
         Assertions.assertEquals(10, wines.size());
     }
-//
-//    @Test
-//    public void testThing() {
-//        Importable<Wine> importer = new WineCSVImporter();
-//        URL url = Thread.currentThread().getContextClassLoader().getResource("files/EmptyCSV.csv");
-//        File file = new File(url.getPath());
-//        List<String[]> lines = importer.readSixLinesFromFile(file);
-//        for (String[] line :lines) {
-//            System.out.println(Arrays.toString(line));
-//        }
-//        Assertions.assertEquals(10, 10);
-//    }
+
+    @Test
+    public void testReadSixLinesFromFile() {
+        Importable<Wine> importer = new WineCSVImporter();
+        URL url = Thread.currentThread().getContextClassLoader().getResource("files/TestDecanter.csv");
+        File file = new File(url.getPath());
+        List<String[]> expectedLines = getSixLinesFromTestDecanter();
+        List<String[]> lines = importer.readSixLinesFromFile(file);
+        for (int i = 0; i < lines.size(); i++) {
+            Assertions.assertArrayEquals(expectedLines.get(i), lines.get(i));
+        }
+    }
 }
