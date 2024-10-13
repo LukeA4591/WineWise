a93de08b74c27fe0392995b449e75207c465d8fc
Alex Wilson <awi218@uclive.ac.nz>
2024-10-13 23:11:10 +1300

    WineWise Deliverable_3.0 Final Commit

--- src/main/java/seng202/team7/gui/AdminScreenController.java
+++ src/main/java/seng202/team7/gui/AdminScreenController.java
@@ -250,7 +250,7 @@ public class AdminScreenController {
             Stage modalStage = new Stage();
             modalStage.setScene(modalScene);
             modalStage.setWidth(900);
-            modalStage.setHeight(624);
+            modalStage.setHeight(655);
             modalStage.setResizable(false);
             modalStage.setTitle("View Wines");
             modalStage.initModality(Modality.WINDOW_MODAL);
